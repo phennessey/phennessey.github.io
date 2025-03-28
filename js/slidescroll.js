8c637da9faa8819ef7dff90a3f11cdd019e9aa28
phennessey <1308575+phennessey@users.noreply.github.com>
2025-03-27 23:32:12 -0700

    Update slidescroll.js

--- js/slidescroll.js
+++ js/slidescroll.js
@@ -5,19 +5,19 @@ $(document).ready(function() {
 		closeEasing : 'easeInBack',
     });
 
-	$('.grid_12').mousemove(function(e) {
+//	$('.grid_12').mousemove(function(e) {
 	
 		//Sidebar Offset, Top value
-		var s_top = parseInt($('.grid_12').offset().top);		
+//		var s_top = parseInt($('.grid_12').offset().top);		
 			
 		//Height of the scrollable area
-		var mheight = 500;
+//		var mheight = 500;
 				
 		//Calculate the top value
-		var top_value = Math.round(( (s_top - e.pageY) / 100) * mheight / 7);
+//		var top_value = Math.round(( (s_top - e.pageY) / 100) * mheight / 7);
 		
 		//Animate the div by chaging the top value
-		$('.scrollme').animate({top: top_value}, { queue:false, duration:800});
-	});
+//		$('.scrollme').animate({top: top_value}, { queue:false, duration:800});
+//	});
 		
 });
