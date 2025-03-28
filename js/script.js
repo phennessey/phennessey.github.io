4fe4df9441b35a764a774e23d99a2e79b9a07179
phennessey <1308575+phennessey@users.noreply.github.com>
2025-03-27 23:10:20 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $("body").queryLoader2({
+        barColor: "#6e6d73",
+        backgroundColor: "#fff1b0",
+        percentage: true,
+        barHeight: 30,
+        completeAnimation: "grow"
+    });
+});
+
+/*
+window.addEventListener('DOMContentLoaded',function() {
+    $("body").queryLoader2({
+        barColor: "#6e6d73",
+        backgroundColor: "#fff1b0",
+        percentage: true,
+        barHeight: 30,
+        completeAnimation: "grow"
+    });
+});
+*/
---
+++ js/slidescroll.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {	
+	
+	$("a[rel=slideshow]").fancybox({
+    	openEasing 	: 'easeOutBack',
+		closeEasing : 'easeInBack',
+    });
+
+	$('.grid_12').mousemove(function(e) {
+	
+		//Sidebar Offset, Top value
+		var s_top = parseInt($('.grid_12').offset().top);		
+			
+		//Height of the scrollable area
+		var mheight = 500;
+				
+		//Calculate the top value
+		var top_value = Math.round(( (s_top - e.pageY) / 100) * mheight / 7);
+		
+		//Animate the div by chaging the top value
+		$('.scrollme').animate({top: top_value}, { queue:false, duration:800});
+	});
+		
+});
