43f69c1bfc479c6eadfdcb0c89660c0e43712f83
Patrick Hennessey <patrick@cincodesign.com>
2026-06-23 01:54:04 -0400

    Deploy color-picker @ 91af081

--- color-picker/src/pantone.js
+++ color-picker/src/pantone.js
@@ -365,7 +365,7 @@ function buildDisplayOrder(matches, targetHue, markEntry) {
   const seam = (targetHue + 0.5) % 1;
   const keyOf = m => (hueOf(m) - seam + 1) % 1;
   const visual = matches.slice().sort((a, b) => keyOf(a) - keyOf(b));
-  const displayOrder = visual.slice().reverse();
+  const displayOrder = visual.slice();
 
   return { displayOrder, markDomIdx: displayOrder.indexOf(markEntry) };
 }
